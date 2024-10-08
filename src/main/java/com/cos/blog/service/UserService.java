1fdbc692899df7e49bf4d0c29bfe3319c9116105
lhj2317 <hjoozzang@naver.com>
2024-10-08 15:11:46 +0900

    52강까지 완료_20241008

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,9 +1,11 @@
 package com.cos.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
  
@@ -15,13 +17,16 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder encoder;	
+	
 	@Transactional
 	public void 회원가입(User user) { 
-			userRepository.save(user); 
-	}
-	
-	@Transactional(readOnly = true )  // Select할때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료(정합성유지)
-	public User 로그인(User user) { 
-		return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword()); 
-	}
+		String rawPassword = user.getPassword();   //1234원문
+		String encPassword = encoder.encode(rawPassword);  //해쉬
+		user.setPassword(encPassword);
+		//실제로 DB에 insert 하고 아래에서 return 
+		user.setRole(RoleType.USER);
+		userRepository.save(user); 
+	}	
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -2,10 +2,7 @@ let index={
 	init: function(){
 		$("#btn-save").on("click",()=>{  //function()대신 ()>{}를 사용한 이유는 this를 바인딩하기 위해서!!!
 			this.save();
-		});
-		$("#btn-login").on("click",()=>{  //function()대신 ()>{}를 사용한 이유는 this를 바인딩하기 위해서!!!
-			this.login();
-		});
+		});		
 	},
 	
 	save : function(){
@@ -23,46 +20,20 @@ let index={
 		// ajax가 통신을 성공공하고 서버가 json을 리턴해주면 자동으로 자바오브젝트로 변환해주네요.
 		$.ajax({ 	// 회원가입수행 요청
 			type: "POST",
-			url: "/blog/api/user",
+			url: "/auth/joinProc",
 			data: JSON.stringify(data),  //http body 데이터
 			contentType: "application/json; charset=utf-8",   // body데이터가 어떤 타입인지 (MIME)
 			dataType: "json"  // 요청을 서버로 해서 응답이 왔을때 기본적으로 모든것이 문자열(생긴게 josn이라면 ) => javascript 오브젝트로 변경
 		}).done(function(resp){ 			// 정상이면 실행
 			alert("회원가입이 완료되었습니다.");
 			//console.log(resp);
-			location.href ="/blog";
-		}).fail(function(error){ 			// 실패면 실행
-			alert(JSON.stringify(error));
-		}); 
-		
-	},
-	
-	login : function(){
-		//alert("user의 save함수 호출됨");
-		let data = {
-			username:$("#username").val(),
-			password:$("#password").val() 
-		};
-		
-		//console.log(data);
-		
-		// ajax 호출시 default가 비동기 호출
-		// ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
-		// ajax가 통신을 성공공하고 서버가 json을 리턴해주면 자동으로 자바오브젝트로 변환해주네요.
-		$.ajax({ 	// 
-			type: "POST",
-			url: "/blog/api/user/login",
-			data: JSON.stringify(data),  //http body 데이터
-			contentType: "application/json; charset=utf-8",   // body데이터가 어떤 타입인지 (MIME)
-			dataType: "json"  // 요청을 서버로 해서 응답이 왔을때 기본적으로 모든것이 문자열(생긴게 josn이라면 ) => javascript 오브젝트로 변경
-		}).done(function(resp){ 			// 정상이면 실행
-			alert("로그인이 완료되었습니다."); 
-			location.href ="/blog";
+			location.href ="/";
 		}).fail(function(error){ 			// 실패면 실행
 			alert(JSON.stringify(error));
 		}); 
 		
 	}
+	
 }
 
 index.init();
